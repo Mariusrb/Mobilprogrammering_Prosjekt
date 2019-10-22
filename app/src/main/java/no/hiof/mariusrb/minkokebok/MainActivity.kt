faf82acfb471921a569332ca1dd40bac485f9add
Marius Røthe Bøen <marobo@live.no>
2019-10-22 13:17:28 +0200

    Verifying uid is correct
    Test to see if the correct uid is logged out

--- app/src/main/java/no/hiof/mariusrb/minkokebok/MainActivity.kt
+++ app/src/main/java/no/hiof/mariusrb/minkokebok/MainActivity.kt
@@ -2,6 +2,7 @@ package no.hiof.mariusrb.minkokebok
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
@@ -28,6 +29,7 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId){
             R.id.menu_sign_out -> {
+                Log.d("MainActivity", "Goodbye ${FirebaseAuth.getInstance().uid}")
                 FirebaseAuth.getInstance().signOut()
                 val intent = Intent(this, RegisterScreen::class.java)
                 intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
