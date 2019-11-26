1653e4fb2ccc0a6bf8dfd7f97d3c5584f4006608
Marius Røthe Bøen <marobo@live.no>
2019-11-26 11:39:52 +0100

    Last minute touches
    Some last minute touches

--- app/src/main/java/no/hiof/mariusrb/minkokebok/Login/LoginScreen.kt
+++ app/src/main/java/no/hiof/mariusrb/minkokebok/Login/LoginScreen.kt
@@ -1,19 +1,21 @@
 package no.hiof.mariusrb.minkokebok.Login
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 import no.hiof.mariusrb.minkokebok.Screens.MainPage
 
 
-
-
-
-
 class LoginScreen : AppCompatActivity() {
+    private val INTERNET_PERMISSION_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,22 +29,61 @@ class LoginScreen : AppCompatActivity() {
     }
 
     private fun performLogin() {
-        val email = loginEmail.text.toString()
-        val password = loginPassword.text.toString()
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) {
 
-        if (email.isEmpty() || password.isEmpty()) {
-            Toast.makeText(this, "Please enter Email and Password", Toast.LENGTH_SHORT).show()
-        } else {
-            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener {
-                    if (!it.isSuccessful) return@addOnCompleteListener
-                    val intent = Intent(this, MainPage::class.java)
-                    startActivity(intent)
+            val email = loginEmail.text.toString()
+            val password = loginPassword.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please enter Email and Password", Toast.LENGTH_SHORT).show()
+            } else {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener {
+                        if (!it.isSuccessful) return@addOnCompleteListener
+                        val intent = Intent(this, MainPage::class.java)
+                        startActivity(intent)
                         Toast.makeText(this, "Logged in successfully", Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+                    }
+            }
+        } else {
+            requestInternetPermission()
+        }
+    }
+
+    //Give the user a second chance with a message showing why access is needed.
+    private fun requestInternetPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.INTERNET)){
+            AlertDialog.Builder(this)
+                .setTitle("Permission needed")
+                .setMessage("This permission is needed because of an online database")
+                .setPositiveButton("Ok"){dialog, which ->
+                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.INTERNET), INTERNET_PERMISSION_CODE)
                 }
-                .addOnFailureListener {
-                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+
+                .setNegativeButton("Cancel"){dialog, which ->
+                    dialog.dismiss()
                 }
+                .create().show()
+        }
+        else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.INTERNET), INTERNET_PERMISSION_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == INTERNET_PERMISSION_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
+
